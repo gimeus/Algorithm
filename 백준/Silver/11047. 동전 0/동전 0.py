337873f5e45dbbccaec977c851e04a911cb0951d
Isu Kim <gimeus@naver.com>
2025-01-05 23:39:40 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+coins.sort(reverse=True)
+
+count = 0
+for coin in coins:
+    if k == 0:
+        break
+    count += k // coin
+    k %= coin
+
+print(count)
